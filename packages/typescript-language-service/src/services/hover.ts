a66fb1feeb04fe2773e7e9643667fffaf1554b7f
zhangyuqiang <jadestrong@Jadestrongs-Mac-mini.local>
2022-03-22 23:21:11 +0800

    add log

--- packages/typescript-language-service/src/services/hover.ts
+++ packages/typescript-language-service/src/services/hover.ts
@@ -3,6 +3,7 @@ import * as vscode from 'vscode-languageserver-protocol';
 import * as previewer from '../utils/previewer';
 import * as shared from '@volar/shared';
 import type { TextDocument } from 'vscode-languageserver-textdocument';
+import logger from '@volar/logger';
 
 export function register(
 	languageService: ts.LanguageService,
@@ -11,7 +12,9 @@ export function register(
 	ts: typeof import('typescript/lib/tsserverlibrary'),
 ) {
 	return (uri: string, position: vscode.Position, documentOnly = false): vscode.Hover | undefined => {
+		logger.info('[typescript-language-service hover.ts] doHover');
 		const document = getTextDocument(uri);
+		logger.info(`[typescript-language-service hover.ts] doHover document]: ${document?.getText()}`)
 		if (!document) return;
 
 		const fileName = shared.uriToFsPath(document.uri);
